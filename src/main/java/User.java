eb10996430b7c770681edde5bb7cb080348a904e
Daniel Blake <dblake@spartaglobal.com>
2018-11-05 12:30:38 +0000

    user code refactored

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -4,18 +4,12 @@ public class User {
     private String name;
     private String email;
     private int age;
-    private JSONObject userDetails = new JSONObject();
+    public JSONObject userDetails = new JSONObject();
 
     public User(String name, int age, String email){
-        this.name = name;
-        this.age = age;
-        this.email = email;
-        addToUserDetails();
-    }
 
-    private void addToUserDetails() {
-        userDetails.put("name", name);
-        userDetails.put("age", age);
-        userDetails.put("email", email);
+        userDetails.put("name", this.name = name);
+        userDetails.put("age", this.age = age);
+        userDetails.put("email", this.email = email);
     }
 }
